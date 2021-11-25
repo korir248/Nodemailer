a4e8b213734ebb1718e61eacecdd6e234d281e08
Eugene Korir <korir16071@gmail.com>
2021-11-25 13:11:41 +0300

    Modified controller

--- controllers/mailController.js
+++ controllers/mailController.js
@@ -5,10 +5,16 @@ const sendEmail = (req,res)=>{
     try{
         sendMail(messageObj).then((result,err)=>{
             if(err) res.send(err.message)
-            return res.send(result)
+            return res.send({
+                messaage: "Email was sent",
+                result: result})
     
         })}catch(err){
             console.log(err.message);
+            res.send({
+                message: "An Error occured",
+                error: err.message
+            })
         }
 }
 
